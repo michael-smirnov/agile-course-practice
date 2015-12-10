3faadabdb58344505458bbbeee2824c70372bc96
Aleksey Nesmelov <70508010alex@mail.ru>
2015-12-11 02:40:54 +0300

    add real logger

---
+++ code/nesmelov-aleksey/Infrastructure/src/main/java/ru/unn/agile/Complex/infrastructure/Logger.java
@@ -0,0 +1,61 @@
+package ru.unn.agile.Complex.infrastructure;
+
+import ru.unn.agile.Complex.viewmodel.ILogger;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+public class Logger implements ILogger{
+    private final String fileName;
+    private BufferedWriter writer;
+
+    private String getCurrentTime() {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.TAIWAN);
+        return dateFormat.format(calendar.getTime());
+    }
+
+    public Logger(final String fileName) {
+        this.fileName = fileName;
+
+        writer = null;
+        try {
+            writer = new BufferedWriter(new FileWriter(fileName));
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    @Override
+    public void addToLog(String message) {
+        try {
+            writer.write(getCurrentTime() + " : " + message + "\n");
+            writer.flush();
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    @Override
+    public List<String> getLog() {
+        BufferedReader reader;
+        ArrayList<String> log = new ArrayList<>();
+
+        try {
+            reader = new BufferedReader(new FileReader(fileName));
+            String line = reader.readLine();
+            while (line != null) {
+                log.add(line);
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+
+        return log;
+    }
+}
---
+++ code/nesmelov-aleksey/Infrastructure/src/test/java/ru/unn/agile/Complex/infrastructure/LoggerTest.java
@@ -0,0 +1,51 @@
+package ru.unn.agile.Complex.infrastructure;
+
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertTrue;
+
+public class LoggerTest {
+    private Logger logger;
+    private static final String fileName = "./LoggerTest.log";
+
+    @Before
+    public void setUp() {
+        logger = new Logger(fileName);
+    }
+
+    @Test
+    public void canCreateLogger() {
+        assertNotNull(new Logger(fileName));
+    }
+
+    @Test
+    public void canWriteLog() {
+        logger.addToLog("Test message.");
+
+        assertTrue(logger.getLog().get(0).matches(".* Test message..*" ));
+    }
+
+    @Test
+    public void logIsEmptyIfNotWrite() {
+        assertTrue(logger.getLog().isEmpty());
+    }
+
+    @Test
+    public void canWriteSeveralLogs() {
+        logger.addToLog("Test message 1.");
+        logger.addToLog("Test message 2.");
+
+        assertTrue(logger.getLog().get(0).matches(".* Test message 1..*"));
+        assertTrue(logger.getLog().get(1).matches(".* Test message 2..*"));
+    }
+
+    @Test
+    public void logContainDataAndTime() {
+        logger.addToLog("Test message.");
+
+        assertTrue(logger.getLog().get(0).matches("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}.*"));
+    }
+}
