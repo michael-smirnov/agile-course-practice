0cf77c800c89d83d5e2207bb88d395ad21daa929
Aleksey Nesmelov <70508010alex@mail.ru>
2015-12-13 20:39:57 +0300

    delete unused import

--- code/nesmelov-aleksey/ViewModel/src/test/java/ru/unn/agile/Complex/viewmodel/ViewModelTest.java
+++ code/nesmelov-aleksey/ViewModel/src/test/java/ru/unn/agile/Complex/viewmodel/ViewModelTest.java
@@ -3,9 +3,6 @@ package ru.unn.agile.Complex.viewmodel;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.ArrayList;
-
 import static org.junit.Assert.*;
 
 public class ViewModelTest {
