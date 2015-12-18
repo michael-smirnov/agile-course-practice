a25a1936888378120d41b4ab63b8fab0eda10e9b
Svetlana <dragonrider@dragon>
2015-12-18 03:51:19 +0300

    TxtHypothecLogger was added.

---
+++ code/filicheva-svetlana/Infrastructure/src/main/java/ru/unn/agile/HypothecCalculator/view/TxtHypothecLogger.java
@@ -0,0 +1,68 @@
+package ru.unn.agile.HypothecCalculator.view;
+
+import ru.unn.agile.HypothecCalculator.viewmodel.IHypothecLogger;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+public class TxtHypothecLogger implements IHypothecLogger {
+    private static final String DATE_FORMAT_NOW = "dd-MM-yyyy HH:mm:ss";
+    private final BufferedWriter writer;
+    final String filename;
+
+    public TxtHypothecLogger(final String filename) {
+        this.filename = filename;
+
+        BufferedWriter logWriter = null;
+        try {
+            logWriter = new BufferedWriter(new FileWriter(filename));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        writer = logWriter;
+    }
+
+    private static String currentTime() {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(DATE_FORMAT_NOW, Locale.ENGLISH);
+        String time = simpleDateFormat.format(calendar.getTime());
+        return simpleDateFormat.format(calendar.getTime());
+    }
+
+    @Override
+    public void addMessage(final String message) {
+        try {
+            writer.write(currentTime() + " > " + message);
+            writer.newLine();
+            writer.flush();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public List<String> getLog() {
+        BufferedReader reader;
+        ArrayList<String> log = new ArrayList<String>();
+        try {
+            reader = new BufferedReader(new FileReader(filename));
+            String line = reader.readLine();
+
+            while (line != null) {
+                log.add(line);
+                line = reader.readLine();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return log;
+    }
+}
---
+++ code/filicheva-svetlana/Infrastructure/src/test/java/ru/unn/agile/HypothecCalculator/view/TxtHypothecLoggerTests.java
@@ -0,0 +1,84 @@
+package ru.unn.agile.HypothecCalculator.view;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import static junit.framework.TestCase.assertNotNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.containsString;
+import java.util.regex.Pattern;
+
+import java.util.List;
+
+public class TxtHypothecLoggerTests {
+    private static final String FILENAME
+            = "log/TxtHypothecLoggerTests.log";
+    private TxtHypothecLogger txtHypothecLogger;
+
+    @Before
+    public void setUp() {
+        txtHypothecLogger = new TxtHypothecLogger(FILENAME);
+    }
+
+    @Test
+    public void canCreateLogger() {
+        assertNotNull(txtHypothecLogger);
+    }
+
+    @Test
+    public void canCreateLogFile() {
+        try {
+            new BufferedReader(new FileReader(FILENAME));
+        } catch (FileNotFoundException e) {
+            fail("File " + FILENAME + " wasn't found!");
+        }
+    }
+
+    @Test
+    public void isLogEmptyAtTheStart() {
+        List<String> log = txtHypothecLogger.getLog();
+        assertEquals(0, log.size());
+    }
+
+    @Test
+    public void canWriteLogMessage() {
+        String rightMessage = "Some interesting message";
+
+        txtHypothecLogger.addMessage(rightMessage);
+
+        String actualMessage = txtHypothecLogger.getLog().get(0);
+        assertThat(actualMessage, containsString(rightMessage));
+    }
+
+    @Test
+    public void canWriteSeveralMessages() {
+        final int numberOfMessages = 8;
+        String[] rightMessages = new String[numberOfMessages];
+
+        for (int i = 0; i < numberOfMessages; i++) {
+            rightMessages[i] = "Message №" + i;
+            txtHypothecLogger.addMessage(rightMessages[i]);
+        }
+
+        List<String> actualMessages = txtHypothecLogger.getLog();
+        assertEquals(numberOfMessages, actualMessages.size());
+        for (int i = 0; i < numberOfMessages; i++) {
+            assertThat(actualMessages.get(i), containsString(rightMessages[i]));
+        }
+    }
+
+    @Test
+    public void canWriteTime() {
+        String rightMessage = "Another interesting message";
+
+        txtHypothecLogger.addMessage(rightMessage);
+
+        String actualMessage = txtHypothecLogger.getLog().get(0);
+        assertTrue(Pattern.matches("^\\d{2}-\\d{2}-\\d{4} \\d{2}:\\d{2}:\\d{2} > .*", actualMessage));
+    }
+}
