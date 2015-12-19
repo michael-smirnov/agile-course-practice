915fc26a7ef648591e5757de5311e2550ac93267
KsenyaKochanova <ksenya.kochanova@gmail.com>
2015-12-19 15:04:24 +0300

    added logic fot logger to view

--- code/kochanova-ksenya/View/src/main/java/ru/unn/agile/CurrencyConverter/view/CurrencyConverter.java
+++ code/kochanova-ksenya/View/src/main/java/ru/unn/agile/CurrencyConverter/view/CurrencyConverter.java
@@ -1,5 +1,7 @@
 package ru.unn.agile.CurrencyConverter.view;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -7,6 +9,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import ru.unn.agile.CurrencyConverter.Model.UnitCurrency;
+import ru.unn.agile.CurrencyConverter.infrastructure.CurrencyConverterLogger;
 import ru.unn.agile.CurrencyConverter.viewmodel.ViewModel;
 
 
@@ -24,14 +27,40 @@ public class CurrencyConverter {
 
     @FXML
     void initialize() {
+        viewModel.setLogger(new CurrencyConverterLogger("./CurrencyConverterLogger-lab3.log"));
 
+        final ChangeListener<Boolean> focusChangeListener = new ChangeListener<Boolean>() {
+            @Override
+            public void changed(final ObservableValue<? extends Boolean> observable,
+                                final Boolean valueBefore, final Boolean valueAfter) {
+                viewModel.onFocusChanged(valueBefore, valueAfter);
+            }
+        };
         inputValue.textProperty().bindBidirectional(viewModel.inputValueProperty());
+        inputValue.focusedProperty().addListener(focusChangeListener);
         inputCurrency.valueProperty().bindBidirectional(viewModel.inputUnitProperty());
+        inputCurrency.focusedProperty().addListener(focusChangeListener);
         outputCurrency.valueProperty().bindBidirectional(viewModel.outputUnitProperty());
+        inputCurrency.focusedProperty().addListener(focusChangeListener);
         btnConvert.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(final ActionEvent event) {
 
+                viewModel.convert();
+            }
+        });
+        inputCurrency.valueProperty().addListener(new ChangeListener<UnitCurrency>() {
+            @Override
+            public void changed(final ObservableValue<? extends UnitCurrency> observable,
+                                final UnitCurrency valueBefore,
+                                final UnitCurrency valueAfter) {
+                viewModel.onUnitChanged(valueBefore, valueAfter);
+            }
+        });
+
+        btnConvert.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent event) {
                 viewModel.convert();
             }
         });
